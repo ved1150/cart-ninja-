dff6cfe3cd8a7c2b26d5e6473cbb51a93408d332
ved1150 <vppatel1771@gmail.com>
2022-09-19 20:48:29 +0530

    increse quantity function

--- src/CartItems.js
+++ src/CartItems.js
@@ -1,18 +1,39 @@
 import React from "react"
-
 class CartItem extends React.Component{
+
+    constructor(){
+        super()
+        this.state = {
+            price : 1000,
+            title : "phone" ,
+             qty : 2,
+            //  img =""
+        }
+    }
+        increaseQuantity =() => {
+         console.log(this.state)
+    }
+
     render(){
+        const {price ,Phone ,qty} =this.state
         return(
             <div className="cart-item">
                 <div className="left-block">
-                    <img style={styles.image} />
+                    <img style={styles.image} src={this.state.img}/>
                 </div>
                 <div className="right-block">
-                    <div style={{fontSize : 25}}>Phone</div>
-                    <div style={{color : "#777"}}>Rs 900</div>
-                    <div style={{color : "#777"}}>Qty: 1</div>
+                    <div style={{fontSize : 25}}>{Phone}</div>
+                    <div style={{color : "#777"}}>Rs :{price}</div>
+                    <div style={{color : "#777"}}>Qty : {qty}</div>
                     <div className="cart-item-actions">
-                        {/* button 3 */}
+                        <img 
+                        src="https://cdn-icons-png.flaticon.com/512/1828/1828925.png"
+                        width ="25px" 
+                        className="action-icons"
+                        onClick={this.increaseQuantity}
+                        />
+                        <img src="https://cdn-icons-png.flaticon.com/128/992/992683.png"  width ="25px" className="action-icons" />
+                        <img src="https://cdn-icons-png.flaticon.com/128/3405/3405244.png" width ="25px" className="action-icons" />
                     </div>
                 </div>
             </div>
@@ -25,6 +46,7 @@ const styles ={
         height : 110,
         margin : 110,
         borderRadius : 4,
+        backgroundColor: "#777"
     }
 }
 
